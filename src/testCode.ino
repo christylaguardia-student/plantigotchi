2098deeaa7e53f185d28cd74c5bcff11db58d843
Wendy Beck <webeck@gmail.com>
2018-03-24 14:25:09 -0700

    adding hannahs code

---
+++ src/testCode.ino
@@ -0,0 +1,15 @@
+int analogPin = A0;     // moisture sensor connected to analog pin A0, our ADC pin
+                       // outside leads to ground and +5V
+int val = 0;           // variable to store the value read
+
+void setup()
+{
+  Serial.begin(9600);              //  setup serial
+}
+
+void loop()
+{
+  val = analogRead(analogPin);     // read the input pin
+  Serial.println(val);             // debug value
+  delay(1000);
+}
